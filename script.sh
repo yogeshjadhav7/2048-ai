514e7d9632363310b7794cb64464cc095a57e51c
Yogesh Jadhav <yogse7en@gmail.com>
2018-07-28 16:18:26 +0530

    Tweaked script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
- for i in `seq $1 $2`;
- do
- 	sh run_script.sh games/game$i.csv
- done   
-
+for i in `seq $1 $2`;
+do
+  if [ ! -f games/game$i.csv ]; then
+    sh run_script.sh games/game$i.csv
+  fi
+done
